48812d64cef10d3ccc71a452b5acaa5620293db4
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-06-06 22:57:49 +0530

    Added:Create Address Book Application Project With Welcome Message

---
+++ addressbookapp/src/main/java/com/bridgelabz/addressbookapp/AddressbookappApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookappApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To AddressBook App");
+		SpringApplication.run(AddressbookappApplication.class, args);
+	}
+
+}
