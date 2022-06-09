bc7bb65d02c29237074aa797c942c67c9f26f38a
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-06-10 02:15:33 +0530

    Added:Use Lombok Library To Auto Generate Getter Setter For the DTO

--- addressbookapp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ addressbookapp/src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,11 +1,13 @@
 package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class AddressBookData {
     private int personId;
     private String name;
