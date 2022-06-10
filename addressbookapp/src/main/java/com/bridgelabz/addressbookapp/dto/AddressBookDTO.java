7869b283402e3f3da57d6f6ebd4ee2b0a79eb262
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-06-10 12:28:32 +0530

    Added:Validation for Name And Phone No Using @Valid For Create and Update REST Call

--- addressbookapp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ addressbookapp/src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -2,9 +2,15 @@ package com.bridgelabz.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class AddressBookDTO {
+    @javax.validation.constraints.Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Addressbook name is Invalid")
+    @NotEmpty(message = "Addressbook name cannot be null")
     public String name;
+    @Pattern(regexp = "\"^[0-9]{0,2}[-][0-9]{10}\"" ,message = "phonenumber is invalid")
     public String phNumber;
 
     public AddressBookDTO(String name, String phNumber) {
