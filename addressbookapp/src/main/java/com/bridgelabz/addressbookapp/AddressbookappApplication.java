813cf7656c906a00badeeb9c377d2816000056eb
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-06-10 12:01:08 +0530

    Added:Set The Enviroment By Updating Configuration

--- addressbookapp/src/main/java/com/bridgelabz/addressbookapp/AddressbookappApplication.java
+++ addressbookapp/src/main/java/com/bridgelabz/addressbookapp/AddressbookappApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbookapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
@@ -10,8 +11,12 @@ public class AddressbookappApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome To AddressBook App");
-		SpringApplication.run(AddressbookappApplication.class, args);
+		System.out.println("Welcome To Address Book Application");
+		ApplicationContext context = SpringApplication.run(AddressbookappApplication.class, args);
+		log.info("Addressbook App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info("AddressBook App DB User is {}",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 		log.info("Addressbook App started");
 	}
 
